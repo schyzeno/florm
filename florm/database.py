d8df0500b1161869c9b6bbdc3603e3168d2be429
schyzeno <namphan00@gmail.com>
2016-07-28 02:28:59 -0500

    finished declarative examples

--- florm/database.py
+++ florm/database.py
@@ -1,11 +1,17 @@
-from sqlalchemy import create_engine, MetaData
+from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('sqlite:////tmp/test.db', convert_unicode=True)
-metadata = MetaData()
+engine = create_engine('sqlite:///florm.db', echo=True,convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
-                                         autoflush=False,
+                                         autoflush=True,
                                          bind=engine))
-def init_db():
-    metadata.create_all(bind=engine)
+Base = declarative_base()
+Base.query = db_session.query_property()
 
+def init_db():
+    # import all modules here that might define models so that
+    # they will be registered properly on the metadata.  Otherwise
+    # you will have to import them first before calling init_db()
+    import florm.models
+    Base.metadata.create_all(bind=engine)
