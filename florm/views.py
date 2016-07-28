ff26465e180b7afec51fa87cab938fdd7fa6309f
Nam Phan <namphan00@gmail.com>
2016-07-28 17:30:04 -0500

    end of day

--- florm/views.py
+++ florm/views.py
@@ -1,5 +1,5 @@
 from florm import app
-from flask import render_template
+from flask import render_template, g,request,url_for, redirect, flash 
 from florm.database import db_session
 from florm.models import User
 
@@ -15,3 +15,13 @@ def index():
 def userlist():
     result = db_session.query(User).all()
     return render_template('userlist.html',message='See my child',users=result)
+
+@app.route('/form')
+def userform():
+    return render_template('form.html',message='See user form')
+
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    db_session.add(User(name=request.form['name'],email=request.form['email']))
+    db_session.commit()
+    return redirect(url_for('userlist')) 
