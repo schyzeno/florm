cb69e92ba08fcb7486549b98f6ecd78e4d46f5f1
Nam Phan <namphan00@gmail.com>
2016-07-28 16:05:20 -0500

    Retrieve from db and print list

--- florm/views.py
+++ florm/views.py
@@ -1,6 +1,7 @@
 from florm import app
 from flask import render_template
 from florm.database import db_session
+from florm.models import User
 
 @app.teardown_appcontext
 def shotdown_session(exception=None):
@@ -9,3 +10,8 @@ def shotdown_session(exception=None):
 @app.route('/')
 def index():
     return render_template('layout.html',message='Hello World!')
+
+@app.route('/users')
+def userlist():
+    result = db_session.query(User).all()
+    return render_template('userlist.html',message='See my child',users=result)
